ff6c958f4725eb998a2f5b78bf40c011d4d3d6c6
sonkm3 <sonkm3@gmail.com>
2021-02-23 13:58:30 +0900

    update queuing method.

--- cranehook.py
+++ cranehook.py
@@ -15,21 +15,19 @@ formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(messag
 logger = logging.getLogger("cranehook")
 logger.setLevel(settings.LOG_LEVEL)
 
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(formatter)
+stream_handler.setLevel(logging.DEBUG)
+
 discord_handler = DiscordHandler(settings.DISCORD_WEBHOOK_URL, "cranehook")
 discord_handler.setFormatter(formatter)
-# logger.addHandler(discord_handler)
-queue_listner = QueueListener(queue, discord_handler, respect_handler_level=True)
+discord_handler.setLevel(logging.INFO)
+queue_listner = QueueListener(queue, discord_handler, stream_handler, respect_handler_level=True)
 
 queue_handler = QueueHandler(queue)
 logger.addHandler(queue_handler)
 
 queue_listner.start()
-
-stream_handler = logging.StreamHandler()
-stream_handler.setFormatter(formatter)
-stream_handler.setLevel(logging.DEBUG)
-logger.addHandler(stream_handler)
-
 
 app = Bottle()
 app.catchall = False
